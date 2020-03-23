1a64a82ce45185db375fae04d30f50a21bdc105c
Yossari4n <jakubstokowski@tutanota.com>
2020-03-23 14:33:51 +0100

    Create basic demo

--- example/main.cpp
+++ example/main.cpp
@@ -1,3 +1,15 @@
+#include "MainScene.h"
+
+#include <Zephyr3D/init.h>
+
 int main() {
+    init();
+
+    MainScene main_scene;
+    main_scene.Initialize();
+    main_scene.CreateScene();
+    main_scene.Run();
+    main_scene.Destroy();
+
     return 0;
 }
